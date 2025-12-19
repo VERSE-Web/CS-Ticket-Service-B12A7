529db2693342e18be28c6f1ab8a7b12423242969
VERSE-Web <Verserxxx@proton.me>
2025-12-19 22:54:25 +0600

    IDK update 2

--- src/components/tickets.jsx
+++ src/components/tickets.jsx
@@ -10,19 +10,19 @@ const Tickets = ({ ticketsPromise }) => {
           <div className='ticketContainer'>
             <div className='top-rank'>
               <h3>{t["#0001"].title}</h3>
-              <h4>{t["#0001"].status}</h4>
+              <h4 className='G-status'>{t["#0001"].status}</h4>
             </div>
             <div className='mid-rank'>
-              <h4>{t["#0001"].description}</h4>
+              <h4 className='grey'>{t["#0001"].description}</h4>
             </div>
             <div className='lowest-rank'>
               <div className='LR-left'>
-                <h5>{t["#0001"].id}</h5>
-                <h5>{t["#0001"].status}</h5>
+                <h6 className='grey'>#{t["#0001"].id}</h6>
+                <h6 className='R-priority'>{t["#0001"].priority}</h6>
               </div>
-              <div>
-                <h5>{t["#0001"].customer}</h5>
-                <h5>{t["#0001"].createdAt}</h5>
+              <div className='LR-right'>
+                <h6 className='grey'>{t["#0001"].customer}</h6>
+                <h6 className='grey'>{t["#0001"].createdAt}</h6>
               </div>
             </div>
           </div>
@@ -32,3 +32,24 @@ const Tickets = ({ ticketsPromise }) => {
 };
 
 export default Tickets;
+
+
+
+          // <div className='ticketContainer'>
+          //   <div className='top-rank'>
+          //     <h3>{t["#0001"].title}</h3>
+          //     <h4 className='G-status'>{t["#0001"].status}</h4>
+          //   </div>
+          //   <div className='mid-rank'>
+          //     <h4 className='desc'>{t["#0001"].description}</h4>
+          //   </div>
+          //   <div className='lowest-rank'>
+          //     <div className='LR-left'>
+          //       <h5 className='id'>{t["#0001"].id}</h5>
+          //       <h5>{t["#0001"].status}</h5>
+          //     </div>
+          //     <div className='LR-right'>
+          //       <h5>{t["#0001"].customer}</h5>
+          //       <h5>{t["#0001"].createdAt}</h5>
+          //     </div>
+          //   </div>
